d9f47f3d3938306fcbdcbfb4d3ec572635cc90bb
AbdiMD <50539805+AbdiMD@users.noreply.github.com>
2024-11-24 18:00:33 +0700

    update with allure report

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['@estruyf/github-actions-reporter']
+    ['line'], ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
